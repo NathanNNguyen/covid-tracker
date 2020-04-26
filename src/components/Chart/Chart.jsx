52bf856cc030390f481394e2563a0e9ab7679f65
Nhan <natenguyen51294@gmail.com>
2020-04-25 17:45:28 -0700

    added barChart when selecting a country

--- src/components/Chart/Chart.jsx
+++ src/components/Chart/Chart.jsx
@@ -4,7 +4,7 @@ import { Line, Bar } from 'react-chartjs-2';
 
 import styles from './Chart.module.scss';
 
-const Chart = () => {
+const Chart = ({ data: { confirmed, recovered, deaths }, country }) => {
   const [dailyData, setDailyData] = useState([]);
 
   useEffect(() => {
@@ -37,11 +37,37 @@ const Chart = () => {
       />) : null
   );
 
+  console.log(confirmed, recovered, deaths)
+
+  const barChart = (
+    confirmed
+      ? (
+        <Bar
+          data={{
+            labels: ['Infected', 'Recover', 'Deaths'],
+            datasets: [{
+              label: 'People',
+              backgroundColor: [
+                'rgba(0, 0, 255, 0.5)',
+                'rgba(0, 255, 0, 0.5)',
+                'rgba(255, 0, 0, 0.5)',
+              ],
+              data: [confirmed, recovered, deaths]
+            }]
+          }}
+          options={{
+            legend: { display: false },
+            title: { display: true, text: `Current state in ${country}` }
+          }}
+        />
+      ) : null
+  )
+
 
 
   return (
     <div className={styles.container}>
-      {lineChart}
+      {country ? barChart : lineChart}
     </div>
   )
 }
